cffd98e8d91285289c74915e29ee6a4fb0fdb0bb
Suyambulingam95 <148951171+Suyambulingam95@users.noreply.github.com>
2023-10-25 13:54:01 +0530

    Add files via upload

---
+++ setting.py
@@ -0,0 +1,16 @@
+level_map = [
+'                            ',
+'                            ',
+'                            ',
+' xx    xxx           xx     ',
+' xx p                        ',
+' xxxx         xx        xx  ',
+' xxxx       xx              ',
+' xx    x  xxxx    xx xx     ',
+'       x  xxxx    xx xxx    ',
+'     xxx  xxxxxx  xx xxxx   ',
+'xxxxxxxx  xxxxxx  xx xxxx   ',]
+
+tile_size = 64
+screen_width = 1200
+screen_height = len(level_map) * tile_size
